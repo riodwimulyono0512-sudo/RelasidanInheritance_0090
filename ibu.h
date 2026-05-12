fdbd4457469f93211de24417505fd0c4d22b8d41
riodwimulyono0512-sudo <riodwimulyono0512@gmail.com>
2026-05-12 21:00:59 +0700

    9

--- ibu.h
+++ ibu.h
@@ -34,3 +34,10 @@ void ibu::cetakAnak() {
     cout << "Daftar Anak dari Ibu \"" << this->nama << "\":\n";
     for (int i = 0; i < daftar_anak.size(); i++) 
     {
+         cout << daftar_anak[i]->nama << endl;
+    }
+    
+    cout << endl;
+}
+
+#endif
