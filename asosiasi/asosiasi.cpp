fb0920dbe42afda56f74850ace6496c44ac75c3a
riodwimulyono0512-sudo <riodwimulyono0512@gmail.com>
2026-05-12 21:05:08 +0700

    1

---
+++ asosiasi/asosiasi.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class dokter;
+
+class pasien { // asosiasi
+public:
+    string nama;
+    vector<dokter*> daftar_dokter;
+    pasien(string pNama) :nama(pNama) 
+    {
+        cout << "Pasien \"" << nama << "\" ada\n";
+    }
