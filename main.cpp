8c910f995055f08c755ee1499814f930755c2f50
riodwimulyono0512-sudo <riodwimulyono0512@gmail.com>
2026-05-12 21:02:28 +0700

    3

--- main.cpp
+++ main.cpp
@@ -4,3 +4,8 @@ using namespace std;
 #include "ibu.h"
 
 int main() { // agregasi
+     ibu* varIbu  = new ibu("dini");
+    ibu* varibu2 = new ibu("novi");
+    anak* varAnak1 = new anak("tono");
+    anak* varAnak2 = new anak("rini");
+    anak* varAnak3 = new anak("Dewi");
